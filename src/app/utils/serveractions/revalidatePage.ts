e0b24cda9d8ea7bb2db6697a91cfaf63e977c051
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2025-01-31 09:06:31 +0100

    make server action async

--- src/app/utils/serveractions/revalidatePage.ts
+++ src/app/utils/serveractions/revalidatePage.ts
@@ -1,7 +1,7 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
+import { revalidatePath } from "next/cache";
 
-export const revalidatePage = (path: string) => {
-    revalidatePath(path)
-}
+export const revalidatePage = async (path: string) => {
+  revalidatePath(path);
+};
