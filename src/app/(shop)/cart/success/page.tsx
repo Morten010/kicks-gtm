bfeb1e58056f2450d8a47c1be977e7e31dcf7946
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2025-01-31 10:58:00 +0100

    change gtm purchase event location

--- src/app/(shop)/cart/success/page.tsx
+++ src/app/(shop)/cart/success/page.tsx
@@ -10,38 +10,6 @@ export default async function SuccessPage() {
 
 
     useEffect(() => {
-        sendGTMEvent({
-            event: 'purchase',
-            data: {
-                transaction_id: "T_12345_1",
-                value: 30.03,
-                tax: 4.90,
-                shipping: 5.99,
-                currency: "USD",
-                items: [
-                    {
-                        item_id: "SKU_12345",
-                        item_name: "Stan and Friends Tee",
-                        affiliation: "Google Merchandise Store",
-                        coupon: "SUMMER_FUN",
-                        discount: 2.22,
-                        index: 0,
-                        item_brand: "Google",
-                        item_category: "Apparel",
-                        item_category2: "Adult",
-                        item_category3: "Shirts",
-                        item_category4: "Crew",
-                        item_category5: "Short sleeve",
-                        item_list_id: "related_products",
-                        item_list_name: "Related Products",
-                        item_variant: "green",
-                        price: 10.01,
-                        quantity: 3
-                    }
-                ]
-            }
-
-        })
         if (cart) {
             clearCart()
         }
--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -1,55 +1,89 @@
 "use client"
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useCart } from '../app/store/useCart'
 import { formatPrice } from '../app/utils/formatPrice'
 import { CartProduct } from '@/types'
 import { useZustand } from '../app/store/useZustand'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
+import { sendGTMEvent } from '@next/third-parties/google'
 
 type SummaryProps = {
   whiteBg?: boolean
 }
 
-export default function OrderSummary({whiteBg = false}: SummaryProps) {
+export default function OrderSummary({ whiteBg = false }: SummaryProps) {
   const cart = useZustand(useCart, (state) => state)
   const router = useRouter()
   const [loading, setLoading] = useState(false)
   const user = useSession()
 
-  if(cart){
+  if (cart) {
   }
   const handleCheckout = async () => {
     setLoading(true)
 
-    if(!cart.cart){
+    if (!cart.cart) {
       setLoading(false)
       return
     }
 
-    
+
     const response = await fetch("/api/stripe", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          cart: cart.cart,
-          userId: user.data?.user.id
-        })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        cart: cart.cart,
+        userId: user.data?.user.id
+      })
     })
 
-    if((response as any).statusCode === 500){
+    if ((response as any).statusCode === 500) {
       setLoading(false)
       return null;
     }
 
     const data = await response.json()
 
-    if(!data.url){
+    if (!data.url) {
       setLoading(false)
       return null
     }
+
+    sendGTMEvent({
+      event: 'purchase',
+      data: {
+        transaction_id: "T_12345_1",
+        value: 30.03,
+        tax: 4.90,
+        shipping: 5.99,
+        currency: "USD",
+        coupon: "SUMMER_SALE",
+        items: [
+          {
+            item_id: "SKU_12345",
+            item_name: "Stan and Friends Tee",
+            affiliation: "Google Merchandise Store",
+            coupon: "SUMMER_FUN",
+            discount: 2.22,
+            index: 0,
+            item_brand: "Google",
+            item_category: "Apparel",
+            item_category2: "Adult",
+            item_category3: "Shirts",
+            item_category4: "Crew",
+            item_category5: "Short sleeve",
+            item_list_id: "related_products",
+            item_list_name: "Related Products",
+            item_variant: "green",
+            location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+            price: 10.01,
+            quantity: 3
+          }]
+      }
+    })
 
     router.push(data.url)
 
@@ -57,29 +91,29 @@ export default function OrderSummary({whiteBg = false}: SummaryProps) {
 
   return (
     <div
-    className={`${whiteBg ? "bg-white" : ""} p-4 rounded-lg w-full flex flex-col gap-2`}
+      className={`${whiteBg ? "bg-white" : ""} p-4 rounded-lg w-full flex flex-col gap-2`}
     >
-        <h2
+      <h2
         className='text-2xl font-semibold'
-        >
+      >
         Order Summary
-        </h2>
-        {/* order summary products list */}
-        <div>
+      </h2>
+      {/* order summary products list */}
+      <div>
         {cart && cart.cart.map((p: CartProduct) => (
-            <div
+          <div
             key={p.id + "_" + p.size}
             className='flex w-full justify-between gap-2'
-            >
-              <p>
-                  {p.amount}x {p.name}
-              </p>
-              <p>{formatPrice(p.price * p.amount)}</p>
-            </div>
+          >
+            <p>
+              {p.amount}x {p.name}
+            </p>
+            <p>{formatPrice(p.price * p.amount)}</p>
+          </div>
         ))}
 
         <div
-        className='flex justify-between text-xl font-semibold my-2'
+          className='flex justify-between text-xl font-semibold my-2'
         >
           <h3>Total</h3>
           <p>{formatPrice(cart ? cart.totalPrice : 0)}</p>
@@ -87,22 +121,22 @@ export default function OrderSummary({whiteBg = false}: SummaryProps) {
 
         {!loading && (
           <button
-          onClick={handleCheckout}
-          className='secondary-btn w-full text-center'
+            onClick={handleCheckout}
+            className='secondary-btn w-full text-center'
           >
-              Review And Pay
+            Review And Pay
           </button>
         )}
         {loading && (
           <button
-          disabled
-          className='secondary-btn w-full text-center opacity-80'
+            disabled
+            className='secondary-btn w-full text-center opacity-80'
           >
-              Redirecting...
+            Redirecting...
           </button>
         )}
-        
-        </div>
+
+      </div>
     </div>
   )
 }
